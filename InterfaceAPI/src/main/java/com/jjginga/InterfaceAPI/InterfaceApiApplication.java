db07584e3d76862a8f3e05544bdcfd8419a0e5e5
joel <jjginga@gmail.com>
2024-05-01 21:41:58 +0100

    Added Interface Module Added Eureka Improvements in Training Plan (training Session concept)

---
+++ InterfaceAPI/src/main/java/com/jjginga/InterfaceAPI/InterfaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.jjginga.InterfaceAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceApiApplication.class, args);
+	}
+
+}
---
+++ InterfaceAPI/src/test/java/com/jjginga/InterfaceAPI/InterfaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjginga.InterfaceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
